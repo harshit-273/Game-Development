cc98e50ebdc7c3672deaf6b9473305d0f7751864
harshit-273 <kalavadiaharshit@gmail.com>
2022-07-04 00:09:33 +0530

    adding images to display added

--- PyGame/SpaceInvader/003_addingImages.py
+++ PyGame/SpaceInvader/003_addingImages.py
@@ -4,16 +4,25 @@ pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
 
-# title and icon
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('icon.png')
 pygame.display.set_icon(icon)
 
+# Player
+playerImg = pygame.image.load('player.png')
+playerX = 368
+playerY = 530
+
+def player():
+	screen.blit(playerImg, (playerX, playerY))
+
 running = True
 while running:
+	screen.fill((32, 32, 32))
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
-    # screen color
-	screen.fill((32, 32, 32))
+	
+	# adding player
+	player()
 	pygame.display.update()
