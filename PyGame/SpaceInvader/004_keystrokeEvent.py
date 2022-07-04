aec9debecc7e5870fb26bb30426e8285aa0ddecf
harshit-273 <kalavadiaharshit@gmail.com>
2022-07-04 12:01:19 +0530

    keystroke events added

--- PyGame/SpaceInvader/004_keystrokeEvent.py
+++ PyGame/SpaceInvader/004_keystrokeEvent.py
@@ -8,10 +8,11 @@ pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('icon.png')
 pygame.display.set_icon(icon)
 
-# Player
 playerImg = pygame.image.load('player.png')
 playerX = 368
 playerY = 530
+playerX_change = 0
+playerY_change = 0
 
 def player(x, y):
 	screen.blit(playerImg, (x, y))
@@ -22,7 +23,17 @@ while running:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
-	
-	# adding player
+		
+		# keystroke events
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_LEFT:
+				playerX_change = -0.2
+			if event.key == pygame.K_RIGHT:
+				playerX_change = 0.2
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+				playerX_change = 0
+
+	playerX += playerX_change
 	player(playerX, playerY)
 	pygame.display.update()
