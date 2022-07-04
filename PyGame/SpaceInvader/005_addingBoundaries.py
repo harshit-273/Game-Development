e2d16167b4c6f88d2165eeb03d1d4c803dde1176
harshit-273 <kalavadiaharshit@gmail.com>
2022-07-04 12:12:10 +0530

    boundaries added

---
+++ PyGame/SpaceInvader/005_addingBoundaries.py
@@ -0,0 +1,45 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('icon.png')
+pygame.display.set_icon(icon)
+
+playerImg = pygame.image.load('player.png')
+playerX = 368
+playerY = 530
+playerX_change = 0
+playerY_change = 0
+
+def player(x, y):
+	screen.blit(playerImg, (x, y))
+
+running = True
+while running:
+	screen.fill((32, 32, 32))
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+		
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_LEFT:
+				playerX_change = -0.2
+			if event.key == pygame.K_RIGHT:
+				playerX_change = 0.2
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+				playerX_change = 0
+
+	playerX += playerX_change
+
+	# adding boundaries
+	if playerX >= 736:
+		playerX = 736
+	elif playerX <= 0:
+		playerX = 0
+
+	player(playerX, playerY)
+	pygame.display.update()
